8b78368e3f2f088f19f260d3994f956a5119d4c9
caelangm <62913521+caelangm@users.noreply.github.com>
2020-04-23 15:46:35 -0400

    changes kingChecker toString

--- src/finalproject/KingChecker.java
+++ src/finalproject/KingChecker.java
@@ -5,5 +5,14 @@ public class KingChecker extends Checker {
 	public KingChecker(int x, int y, boolean color) {
 		super(x,y,color);
 	}
+	
+	public String toString(){
+		if (color) {
+			return "X";
+		} else {
+			return "O";
+		}
+	}
+	
 
 }
