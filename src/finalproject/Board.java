c599b4502fc8afccec772588c283f1afc425fb06
denisezhong <62267168+denisezhong@users.noreply.github.com>
2020-04-23 15:06:56 -0400

    printBoard method

--- src/finalproject/Board.java
+++ src/finalproject/Board.java
@@ -4,12 +4,20 @@ public class Board {
 	
 	public void printBoard() {
 		for(int i=0; i<8; i++) {
-			for(int j=0; j<8; j++) {
-				String s = "| ";
-				System.out.printf("%s", s);
+			for(int j=0; j<4; j++) {
+				if(i%2 ==0) {
+					String s = "| |_";
+					System.out.printf("%s", s);
+				}
+				else {
+					String s = "|_| ";
+					System.out.printf("%s", s);
+				}
+				
 			}
-			System.out.println();
+			System.out.println("|"+i);
 		}
+		System.out.printf("%s"," 0 1 2 3 4 5 6 7");
 	}
 	
 }
