b7bf7cdea271a6c63b3651ec9fa314f6a5811efe
denisezhong <62267168+denisezhong@users.noreply.github.com>
2020-04-23 15:29:44 -0400

    Bigger Board

--- src/finalproject/Board.java
+++ src/finalproject/Board.java
@@ -3,21 +3,26 @@ package finalproject;
 public class Board {
 	
 	public void printBoard() {
-		for(int i=0; i<8; i++) {
-			for(int j=0; j<4; j++) {
-				if(i%2 ==0) {
-					String s = "| |_";
+		for(int i=0; i<16; i++) {
+			for(int j=0; j<8; j++) {
+				if(i%2 == 0) {
+					String s = "|   ";
 					System.out.printf("%s", s);
 				}
 				else {
-					String s = "|_| ";
+					String s = "|___";
 					System.out.printf("%s", s);
 				}
 				
 			}
-			System.out.println("|"+i);
+			if (i%2 == 0) {
+				System.out.println("|"+i/2);
+			}
+			else {
+				System.out.println("|");
+			}
 		}
-		System.out.printf("%s"," 0 1 2 3 4 5 6 7");
+		System.out.printf("%s","   0   1   2   3   4   5   6   7");
 	}
 	
 }
