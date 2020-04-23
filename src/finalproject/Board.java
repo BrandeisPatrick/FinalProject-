a5e2f4f1a080385146782f7f0959a7fc9c3e0c73
BrandeisPatrick <59629055+BrandeisPatrick@users.noreply.github.com>
2020-04-23 16:05:16 -0400

    - Board

--- src/finalproject/Board.java
+++ src/finalproject/Board.java
@@ -1,15 +1,49 @@
 package finalproject;
 
 public class Board {
-	
-	public void printBoard() {
-		for(int i=0; i<8; i++) {
-			for(int j=0; j<8; j++) {
-				String s = "| ";
-				System.out.printf("%s", s);
-			}
-			System.out.println();
+
+	public char[][] board;
+
+
+	public Board() {
+		board = new char[9][9];
+		setHeader();
+	}
+
+	public void setHeader(){
+		int decimalCode = 69;
+		for(int i = 1; i <= 8; i++){
+			this.board[0][i] = (char) decimalCode;
+			decimalCode++;
+		}
+
+		decimalCode = 49;
+		for(int i = 1; i <= 8; i++){
+			this.board[i][0] = (char) decimalCode;
+			decimalCode++;
 		}
 	}
-	
+
+
+	public String toString() {
+		String output = "";
+		for (int i = 0; i <= 8; i++) {
+			for (int j = 0; j <= 8; j++) {
+				if(this.board[i][j] == '\u0000'){
+					output += "_" + "    ";
+				}else {
+					output += this.board[i][j] +  "    ";
+				}
+			}
+			output += "\n\n" ;
+		}
+		return output;
+	}
+
+	public static void main(String[] args){
+		Board board = new Board();
+		System.out.print(board);
+	}
+
+
 }
