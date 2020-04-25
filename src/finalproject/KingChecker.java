93e880c800a754745062ab2d41429510f7795933
denisezhong <62267168+denisezhong@users.noreply.github.com>
2020-04-25 17:46:51 -0400

    canMove method in KingChecker class

--- src/finalproject/KingChecker.java
+++ src/finalproject/KingChecker.java
@@ -11,8 +11,23 @@ public class KingChecker extends Checker {
 	}
 	
 	@Override
+	//checks if the checker can move to that spot
 		public boolean canMove(int xCoordinate, int yCoordinate, ArrayList<Checker> checkers) { //coordinates player is trying to move to
-			
+			if(xCoordinate < 1 || xCoordinate > 8 || yCoordinate < 1 || yCoordinate > 8) {
+				System.out.println("The Coordinates you entered are out of bounds");
+				return false;
+			}else if((xCoordinate == this.x && yCoordinate == this.y + 1)||
+					(xCoordinate == this.x && yCoordinate == this.y - 1) ||
+					(xCoordinate == this.x + 1 && yCoordinate == this.y) ||
+					(xCoordinate == this.x - 1 && yCoordinate == this.y)) {
+				System.out.println("That's an invalid move, you can only move diagonally");
+				return false;
+			}else if(spotTaken(checkers, this.getX(), this.getY())) {
+				System.out.println("That spot is taken");
+				return false;
+			}else {
+				return true;
+			}
 		}
 	
 	public String toString(){
