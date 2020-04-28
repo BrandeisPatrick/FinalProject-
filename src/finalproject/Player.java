2c756347f88eb1f5c028fb8918d00000e2154d95
kyrarivest <57569515+kyrarivest@users.noreply.github.com>
2020-04-27 23:28:29 -0400

    removed redundant code

--- src/finalproject/Player.java
+++ src/finalproject/Player.java
@@ -8,22 +8,16 @@ import java.util.Scanner;
 
 public class Player {
 	public String[] letters = new String[]{"A", "B", "C", "D", "E", "F", "G", "H"};
-
 	public boolean color; //true = x false = o..
-	ArrayList<Checker> checkers;
 	Map<String,Integer> letterToInt;
 	
 	public Player(boolean color) {
-		int checkerNum = 0;
 		this.color = color;
-		this.checkers = new ArrayList<Checker>();
-		setCheckers(color, checkers, checkerNum);
 		setLetterToInt();
 	}
 
 	public Player() {
 		this.color = true;
-		this.checkers = new ArrayList<Checker>();
 	}
 	
 	public void setLetterToInt() {
@@ -32,47 +26,14 @@ public class Player {
 			letterToInt.put(this.letters[i-1],i);
 		}
 	}
-
-	public void setCheckers(boolean color, ArrayList<Checker> checkers, int checkerNum) {
-		if(color == true) { //player x
-			this.color = true;
-			for(int i = 1; i < 4; i++) { //iterates through three rows
-				if(i%2 != 0) {
-					for(int j = 1; j<= 7; j+=2) {
-						checkers.add(new Checker(i-1, j, true, checkerNum +1));
-						checkerNum ++;
-					}
-				}else {
-					for(int j = 0; j<= 6; j+=2) {
-						checkers.add(new Checker(i-1, j, true, checkerNum +1));
-						checkerNum ++;
-					}
-				}
-			}
-		}else{
-			this.color = false;
-			for(int i = 5; i <= 7; i++) {
-				if(i%2 == 0) {
-					for(int j = 0; j<=6; j+=2) {
-						checkers.add(new Checker(i,j, true, checkerNum +1));
-						checkerNum ++;
-					}
-				}else {
-					for(int j = 1; j<=7; j+=2) {
-						checkers.add(new Checker(i,j, true, checkerNum +1));
-						checkerNum ++;
-					}
-				}
-			}
-			
-		}
-	}
 	
 
 	
-	public void printPlayer() {
-		for(Checker c : checkers) {
-			System.out.println( "(" + c.getX() + "," + c.getY() + ")");
+	public String toString() {
+		if(this.color) {
+			return "Player 1";
+		}else {
+			return "Player 2";
 		}
 	}
 	
@@ -83,9 +44,9 @@ public class Player {
 	public static void main(String[] args) {
 		Player pTrue = new Player(true);
 		Player pFalse = new Player(false);
-		pTrue.printPlayer();
+		System.out.println(pTrue);
 		System.out.println();
-		pFalse.printPlayer();
+		System.out.println(pFalse);
 		
 	}
 
