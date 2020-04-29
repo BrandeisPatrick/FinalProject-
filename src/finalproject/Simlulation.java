61819a303b2a0b57ce3fc839052789a399081a31
denisezhong <62267168+denisezhong@users.noreply.github.com>
2020-04-29 01:19:04 -0400

    While loop in play game class

--- src/finalproject/Simlulation.java
+++ src/finalproject/Simlulation.java
@@ -10,7 +10,7 @@ public class Simlulation {
     	printHeader();
         setPlayer();
         board = new Board();
-     // board.printCheckerCoord();
+        board.printCheckerCoord();
     }
 
     //distribute the players
@@ -22,15 +22,15 @@ public class Simlulation {
             System.out.println("Please give a valid answer.");
             str  = sc.next();
         }
+        this.player1 = new Player(true);
+        this.player2 = new Player(false);
         if(str.equals("yes")){
-        	this.player1 = new Player(true);
-            this.player2 = new Player(false);
+        	System.out.println("You are " + this.player1);
+            System.out.println("The other person is " + this.player2);
         }else{
-        	this.player1 = new Player(false);
-            this.player2 = new Player(true);
+        	System.out.println("You are " + this.player2);
+            System.out.println("The other person is " + this.player1);
         }
-        System.out.println("You are " + this.player1);
-        System.out.println("The other person is " + this.player2);
         
     }
 
@@ -41,12 +41,22 @@ public class Simlulation {
         System.out.println("Player O  will move after;");
         System.out.println("In each move, select a checker by giving a coordinate, then give the destination coordinate, separated by a space");
         System.out.println("For example A1 checker move to B2 position is \"A1 B2\"");
+
         System.out.println();
     }
 
-    public void tick(){
+    public boolean tick(){
         player1.tick(this.board.checkersInUse, this.board);
+        if(player1.allPiecesGone(this.board.checkersInUse) == true){
+        	System.out.println("Player "+ this.player1 + "has won!");
+        	return false;
+        }
         player2.tick(this.board.checkersInUse, this.board);
+        if(player2.allPiecesGone(this.board.checkersInUse) == true){
+        	System.out.println("Player "+ this.player2 + "has won!");
+        	return false;
+        }
+        return true;
     }
 
     @Override
