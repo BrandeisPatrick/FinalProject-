8b1972b03af60e1bed75a4f99566fa1f5ed8c820
kyrarivest <57569515+kyrarivest@users.noreply.github.com>
2020-04-30 00:26:06 -0400

    Trying out graphic stuff

---
+++ src/finalproject/GraphicBoard.java
@@ -0,0 +1,14 @@
+package finalproject;
+
+public class GraphicBoard extends Board {
+
+	public GraphicBoard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
